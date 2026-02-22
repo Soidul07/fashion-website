8bf97f04abdbd74aff42f33e5458aae451caa180
soidul7 <soidul@reddensoft.com>
2026-02-22 19:31:09 +0530

    update

--- src/app/Component/Widgets/Homepage/HappyCastomer.js
+++ src/app/Component/Widgets/Homepage/HappyCastomer.js
@@ -1,12 +1,26 @@
 "use client"
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image';
 import Slider from "react-slick";
 import { FaStar } from "react-icons/fa";
-
-import { customer1,customer2,customer3,customer4 } from "../../../assets/index";
+import axios from 'axios';
 
 export default function HappyCastomer() {
+    const [testimonials, setTestimonials] = useState([]);
+
+    useEffect(() => {
+        const fetchTestimonials = async () => {
+            try {
+                const response = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/testimonials`);
+                if (response.data.success) {
+                    setTestimonials(response.data.data);
+                }
+            } catch (error) {
+                console.error("Error fetching testimonials:", error);
+            }
+        };
+        fetchTestimonials();
+    }, []);
 
     var sliderarticles= {
         infinite:true, 
@@ -22,185 +36,75 @@ export default function HappyCastomer() {
     };
 
     return  (
-        <div className='review_sales'>
-            <div className='container'>
-                <div className='row'>
-                    <div className='top_heading'>
-                        <div className='heading'>
-                            <h2>Why they keep coming back</h2>
+        <>
+        {testimonials.length > 0 ? (
+            <div className='review_sales'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='top_heading'>
+                            <div className='heading'>
+                                <h2>Why they keep coming back</h2>
+                            </div>
                         </div>
-                    </div>
-                    <div className='review_sales_slider'>
-                        <Slider  {...sliderarticles}>
-                            <div className='item'>
-                                <div className='padding'>
-                                    <div className='review_boxes'>
-                                        <div className='col-lg-6 col-12'>
-                                            <div className='review_image'>
-                                                <Image 
-                                                    src={customer1} 
-                                                    alt="image" 
-                                                    width={500}
-                                                    height={300}
-                                                />
-                                            </div>
-                                        </div>
-                                        <div className='col-lg-6 col-12'>
-                                            <div className='review_text'>
-                                                <div className='image_box'>
-                                                    <Image 
-                                                        src={customer1} 
-                                                        alt="image" 
-                                                        width={500}
-                                                        height={300}
-                                                    />
+                        <div className='review_sales_slider'>
+                            <Slider {...sliderarticles}>
+                                {testimonials.map((item) => (
+                                    <div className='item' key={item.id}>
+                                        <div className='padding'>
+                                            <div className='review_boxes'>
+                                                <div className='col-lg-6 col-12'>
+                                                    <div className='review_image'>
+                                                        <Image 
+                                                            src={item.image} 
+                                                            alt={item.name} 
+                                                            width={500}
+                                                            height={300}
+                                                        />
+                                                    </div>
                                                 </div>
-                                                <h2>Rhea Sharma</h2>
-                                                <div className='review_star'>
-                                                    <ul>
-                                                        <li><FaStar /></li>
-                                                        <li><FaStar /></li>
-                                                        <li><FaStar /></li>
-                                                        <li><FaStar /></li>
-                                                        <li><FaStar /></li>
-                                                    </ul>
+                                                <div className='col-lg-6 col-12'>
+                                                    <div className='review_text'>
+                                                        <div className='image_box'>
+                                                            <Image 
+                                                                src={item.image} 
+                                                                alt={item.name} 
+                                                                width={500}
+                                                                height={300}
+                                                            />
+                                                        </div>
+                                                        <h2>{item.name}</h2>
+                                                        <div className='review_star'>
+                                                            <ul>
+                                                                <li><FaStar /></li>
+                                                                <li><FaStar /></li>
+                                                                <li><FaStar /></li>
+                                                                <li><FaStar /></li>
+                                                                <li><FaStar /></li>
+                                                            </ul>
+                                                        </div>
+                                                        <p dangerouslySetInnerHTML={{ __html: item.description }} />
+                                                    </div>
                                                 </div>
-                                                <p>
-                                                    Loving Fly's desi wear collection! Comfortable & stylish. Loving Fly's desi wear collection! Comfortable & stylish. Loving Fly's desi wear collection! Comfortable & stylish.
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='item'>
-                                <div className='padding'>
-                                    <div className='review_boxes'>
-                                        <div className='col-lg-6 col-12'>
-                                            <div className='review_image'>
-                                                <Image 
-                                                    src={customer2} 
-                                                    alt="image" 
-                                                    width={500}
-                                                    height={300}
-                                                />
-                                            </div>
-                                        </div>
-                                        <div className='col-lg-6 col-12'>
-                                            <div className='review_text'>
-                                                <div className='image_box'>
-                                                    <Image 
-                                                        src={customer2} 
-                                                        alt="image" 
-                                                        width={500}
-                                                        height={300}
-                                                    />
-                                                </div>
-                                                <h2>Rhea Sharma</h2>
-                                                <div className='review_star'>
-                                                    <ul>
-                                                        <li><FaStar /></li>
-                                                        <li><FaStar /></li>
-                                                        <li><FaStar /></li>
-                                                        <li><FaStar /></li>
-                                                        <li><FaStar /></li>
-                                                    </ul>
-                                                </div>
-                                                <p>
-                                                    Loving Fly's desi wear collection! Comfortable & stylish. Loving Fly's desi wear collection! Comfortable & stylish.
-                                                    Loving Fly's desi wear collection! Comfortable & stylish.
-                                                </p>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-                            <div className='item'>
-                                <div className='padding'>
-                                    <div className='review_boxes'>
-                                        <div className='col-lg-6 col-12'>
-                                            <div className='review_image'>
-                                                <Image 
-                                                    src={customer3} 
-                                                    alt="image" 
-                                                    width={500}
-                                                    height={300}
-                                                />
-                                            </div>
-                                        </div>
-                                        <div className='col-lg-6 col-12'>
-                                            <div className='review_text'>
-                                                <div className='image_box'>
-                                                    <Image 
-                                                        src={customer3} 
-                                                        alt="image" 
-                                                        width={500}
-                                                        height={300}
-                                                    />
-                                                </div>
-                                                <h2>Rhea Sharma</h2>
-                                                <div className='review_star'>
-                                                    <ul>
-                                                        <li><FaStar /></li>
-                                                        <li><FaStar /></li>
-                                                        <li><FaStar /></li>
-                                                        <li><FaStar /></li>
-                                                        <li><FaStar /></li>
-                                                    </ul>
-                                                </div>
-                                                <p>
-                                                    Loving Fly's desi wear collection! Comfortable & stylish.
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='item'>
-                                <div className='padding'>
-                                    <div className='review_boxes'>
-                                        <div className='col-lg-6 col-12'>
-                                            <div className='review_image'>
-                                                <Image 
-                                                    src={customer4} 
-                                                    alt="image" 
-                                                    width={500}
-                                                    height={300}
-                                                />
-                                            </div>
-                                        </div>
-                                        <div className='col-lg-6 col-12'>
-                                            <div className='review_text'>
-                                                <div className='image_box'>
-                                                    <Image 
-                                                        src={customer4} 
-                                                        alt="image" 
-                                                        width={500}
-                                                        height={300}
-                                                    />
-                                                </div>
-                                                <h2>Rhea Sharma</h2>
-                                                <div className='review_star'>
-                                                    <ul>
-                                                        <li><FaStar /></li>
-                                                        <li><FaStar /></li>
-                                                        <li><FaStar /></li>
-                                                        <li><FaStar /></li>
-                                                        <li><FaStar /></li>
-                                                    </ul>
-                                                </div>
-                                                <p>
-                                                    Loving Fly's desi wear collection! Comfortable & stylish.
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </Slider>
+                                ))}
+                            </Slider>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        ) : (
+            <div className='review_sales'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='text-center py-5'>
+                            <p>No data found</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )}
+        </>
     )
 }
