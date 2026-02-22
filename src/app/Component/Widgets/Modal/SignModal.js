8c4812e55589739d6cf4c488d367459d8c63e6c3
soidul7 <soidul@reddensoft.com>
2026-02-22 21:22:31 +0530

    add feature and modify

--- src/app/Component/Widgets/Modal/SignModal.js
+++ src/app/Component/Widgets/Modal/SignModal.js
@@ -70,11 +70,11 @@ export default function SignModal() {
             }
         }
 
-        if (!values.email) {
-            errors.email = 'Email is required';
-        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-            errors.email = 'Invalid email address';
-        }
+        // if (!values.email) {
+        //     errors.email = 'Email is required';
+        // } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        //     errors.email = 'Invalid email address';
+        // }
         return errors;
     };
 
@@ -206,9 +206,7 @@ export default function SignModal() {
                                                 value={removeExtraSpace(formik.values.email)}
                                                 placeholder="Enter Email Id"
                                             />
-                                            {formik.touched.email && formik.errors.email && (
-                                                <p className="error">{formik.errors.email}</p>
-                                            )}
+                                            
                                         </div>
                                         <button type="submit" disabled={leadsBtnLoader}>
                                             {leadsBtnLoader ? 'Submitting...' : 'SUBMIT'}
